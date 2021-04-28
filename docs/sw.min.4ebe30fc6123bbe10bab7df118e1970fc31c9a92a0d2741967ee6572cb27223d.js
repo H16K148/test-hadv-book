91b7c5c43e28cbd63c4a9a7b97b44505adf0b96b
H16K148 <h16k148@gmail.com>
2021-04-28 20:04:17 +0000

    最初のコミット

--- docs/sw.min.4ebe30fc6123bbe10bab7df118e1970fc31c9a92a0d2741967ee6572cb27223d.js
+++
@@ -1,1 +0,0 @@
-navigator.serviceWorker&&navigator.serviceWorker.register("/hadv-book/sw.js",{scope:"/hadv-book/"})
---
+++ docs/sw.min.f3b97e3af5505bb49ff5173b8d593292153b7a52c2f361b8e7b93cd5e3913a14.js
@@ -0,0 +1,1 @@
+navigator.serviceWorker&&navigator.serviceWorker.register("/test-hadv-book/sw.js",{scope:"/test-hadv-book/"})
